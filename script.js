761171f61d0e6819ea40bfcdaa78c7d29e11427c
Manjeet Kumar <manjitmodi5@gmail.com>
2022-07-23 16:44:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const codes= document.querySelectorAll('.code')
+codes[0].focus()
+codes.forEach((code,index) => {
+    code.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <=9){
+            codes[index].value = '';
+            setTimeout(() => codes[index+1].focus(), 10)
+            }
+            else if(e.key === 'Backspace'){
+                setTimeout(() => codes[index-1].focus() ,10)
+                }
+            })
+        })
+
+        let popup = document.getElementById("popup");
+
+    function openPopup(){
+      popup.classList.add("open-popup") 
+    }
+    function closePopup(){
+      popup.classList.remove("open-popup")
+    } 
+       
+
